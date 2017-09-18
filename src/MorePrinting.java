a7fb07a96328cea28a1a296c72222b69341fe59d
KrashMunoz <Krash.Munoz@gmail.com>
2017-09-18 19:23:45 -0400

    Initial commit

---
+++ src/MorePrinting.java
@@ -0,0 +1,22 @@
+public class MorePrinting {
+    public static void main(String[] args){
+        System.out.println("----------------------------------");
+        System.out.println("|   Java Bank ATM Receipt        |");
+        System.out.println("|   Monday, September 18, 2017   |");
+        System.out.println("|   ATM Location #151            |");
+        System.out.println("|                                |");
+        System.out.println("|                                |");
+        System.out.println("|   Account Number:   02752945   |");
+        System.out.println("|   Customer:     Krash Munoz    |");
+        System.out.println("|   Transaction Type:  Deposit   |");
+        System.out.println("|   Transaction Amount: $50.00   |");
+        System.out.println("|   Account Balance: $550.00     |");
+        System.out.println("|                                |");
+        System.out.println("|   Thank you for choosing us    |");
+        System.out.println("|        Have a nice day!        |");
+        System.out.println("|                                |");
+        System.out.println("|                                |");
+        System.out.println("----------------------------------");
+
+    }
+}
